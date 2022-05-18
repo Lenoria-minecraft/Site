e197740a0ac494ab93eb5e94ad74b796aae996b0
Lenoria-minecraft <100515414+Lenoria-minecraft@users.noreply.github.com>
2022-05-18 19:42:49 +0200

    qdzkuyqgzdqjzydg

--- main.js
+++ main.js
@@ -7,3 +7,31 @@ mobile.addEventListener('click',() => {
     menu.classList.toggle("active");
     bodyzd.classList.toggle("active");
 })
+
+
+if( navigator.userAgent.match(/iPhone/i)
+|| navigator.userAgent.match(/webOS/i)
+|| navigator.userAgent.match(/Android/i)
+|| navigator.userAgent.match(/iPad/i)
+|| navigator.userAgent.match(/iPod/i)
+|| navigator.userAgent.match(/BlackBerry/i)
+|| navigator.userAgent.match(/Windows Phone/i)
+){
+
+document.write('<code html pour mobile>');
+
+} else {
+
+document.write('<code html pour ordi>');
+
+}
+
+
+let editor = document.querySelector('.ip');
+let button = document.querySelector('.header-btn me-3');
+
+button.addEventListener('click', () => {
+    editor.select();
+    navigator.clipboard.writeText(editor.value);
+    button.innerText = "Copié !";
+})
