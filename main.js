3d73c4ec3239eb9afa474ac994d3631fd6b00b02
Lenoria-minecraft <100515414+Lenoria-minecraft@users.noreply.github.com>
2022-05-24 18:10:39 +0200

    m

--- main.js
+++
@@ -1,8 +0,0 @@
-let editor = document.querySelector('.ip');
-let button = document.querySelector('.header-btn me-3');
-
-button.addEventListener('click', () => {
-    editor.select();
-    navigator.innerText.writeText(editor.value);
-    button.innerText = "Copié !";
-})
